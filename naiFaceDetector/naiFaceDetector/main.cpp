7606a1a143ace311db63c3b46d608c0647495454
stasqe <stasqe@gmail.com>
2014-06-07 11:13:26 +0200

    obsluga przechwytywania

--- naiFaceDetector/naiFaceDetector/main.cpp
+++ naiFaceDetector/naiFaceDetector/main.cpp
@@ -1,11 +1,36 @@
-#include "main.h"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
+#include <iostream>
+#include <stdio.h>
 
-main::main()
+using namespace std;
+using namespace cv;
+
+// funkcja main
+int main(void)
 {
-}
+	VideoCapture capture(0);
 
+	if (!capture.isOpened())  // sprawdzam czy przechwytywanie zakonczone sukcesem
+		return -1;
 
-main::~main()
-{
+	// odczyt strumienia
+	Mat frame;
+
+	for (;;)
+	{
+		capture >> frame;
+
+		//obsluga przerwania
+		int c = waitKey(10);
+
+		if (27 == char(c)) 
+		{
+			break;
+		}
+	}
+
+	return 0;
 }
